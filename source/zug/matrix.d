57c3a0bd7b3ef54a2f44f65daadda82f164852f5
Emil Nicolaie Perhinschi <emilper@gmail.com>
2018-12-11 01:04:56 +0200

    stretching rows works

--- source/zug/matrix.d
+++ source/zug/matrix.d
@@ -942,68 +942,61 @@ unittest
 T[] stretch_row(T)( T[] orig, size_t new_size ) {
 
     double spacing = ( (new_size.to!double - 1)/( orig.length.to!double - 1 ) );
-    writeln( [ "new_size" : new_size, "orig.length:": orig.length, "spacing:": spacing ], "spacing" );
     double[] stretched_coordinates = new double[orig.length];
     for (size_t i = 0; i < orig.length; i++) {
         stretched_coordinates[i] = i.to!double * spacing;
     }
 
-    writeln(stretched_coordinates, "================================= stretch_row ");
-
-    // deal with floating point weirdnesses, make sure the last value is what
-    //   it should be
+    // deal with floating point weirdnesses, make sure the last value is what it should be
     stretched_coordinates[stretched_coordinates.length -1] = new_size - 1;
 
-    // let orig_coordinates = 0
-    // let next_coordinates = stretched_coordinates[orig_coordinates]
-    // let prev_coordinates = 0
-    // const stretched = Array.from(
-    //     Array(new_size),
-    //     function(undef, i) {
+    size_t orig_coordinates = 0;
+    double next_coordinates = stretched_coordinates[orig_coordinates];
+    double prev_coordinates = 0;
+    T[] stretched = new T[new_size];
 
-    //         if (
-    //             // less than the fractional part
-    //             next_coordinates - i <= (next_coordinates % 1)
-    //         ) {
-    //             const value = orig[orig_coordinates]
-    //             prev_coordinates = next_coordinates
-    //             orig_coordinates += 1
-    //             next_coordinates = stretched_coordinates[orig_coordinates]
-    //             return value
-    //         } else {
-    //             const slope =
-    //                 Number(orig[orig_coordinates] - orig[orig_coordinates - 1])
-    //                 /
-    //                 Number(next_coordinates - prev_coordinates)
+    for (size_t i = 0; i < new_size; i++) {
+        auto blabla = next_coordinates % 1;
+        if (
+            next_coordinates - i <= (next_coordinates % 1)
+        ) {
+            stretched[i] = orig[orig_coordinates];
+            prev_coordinates = next_coordinates;
+            orig_coordinates += 1;
+            if (orig_coordinates < stretched_coordinates.length) {
+                next_coordinates = stretched_coordinates[orig_coordinates];
+            } else {
+                break;
+            }
+        } else {
+            double slope =
+                (orig[orig_coordinates] - orig[orig_coordinates - 1]).to!double
+                /
+                (next_coordinates - prev_coordinates);
 
-    //             if(isNaN(slope)) {
-    //                 console.error("xxx slope is NaN", i, orig_coordinates, orig)
-    //                 throw new Error("something very wrong: slope is NaN")
+            double value =  orig[orig_coordinates - 1].to!double + ( slope * (i - prev_coordinates) ).to!double;
 
-    //             }
+            stretched[i] = value.to!T;
+        }
+    }
 
-    //             const value =  Number(orig[orig_coordinates - 1])
-    //                 + Number( slope * (i - prev_coordinates) )
-
-    //             if(isNaN(value)) {
-    //                 console.error("yyy isNaN", i, slope, orig[orig_coordinates - 1], prev_coordinates, orig)
-    //                 throw new Error("something very wrong: value is NaN")
-    //             }
-    //             return value
-    //         }
-    //     }
-    // )
-    // return stretched
-
-    T[] result;
-    return result;
+    return stretched;
 }
 
 unittest
 {
-    int[] orig = [0,1,2,3,4,5];
-    auto result = stretch_row(orig, 33);
-    
+    import std.algorithm.comparison: equal;
+    float[] orig = [0,1,2,3,4];
+    auto result = stretch_row(orig, 15);
+    // expected [0, 0.285714, 0.571429, 1, 1.14286, 1.42857, 1.71429, 2, 2.28571, 2.57143, 3, 3.14286, 3.42857, 3.71429, 4]
+    // assert(result.equal(expected));
+    // writeln("result", result);
+    assert(result.length == 15);
+    assert(result[0] == orig[0]);
+    assert(result[3] == orig[1]);
+    assert(result[7] == orig[2]);
+    assert(result[10] == orig[3]);
+    assert(result[14] == orig[4]);
 }
 
 /*
