4d288644a239a55f5f84706635112589f433740d
Emil Nicolaie Perhinschi <emilper@gmail.com>
2019-01-13 00:46:05 +0200

    more testing, this time a lot more practical

--- tests/vector_operations.d
+++ tests/vector_operations.d
@@ -9,15 +9,15 @@ void main()
 
 void test_vector() {
 
-    int[10000000] test;
+    int[1000000] test;
     int[] result;
     result[] = test[] + 1;
 }
 
 void test_loop() {
-    int[10000000] test;
-    int[10000000] result;
-    for (size_t i = 0; i < 100000; i++) {
+    int[1000000] test;
+    int[1000000] result;
+    for (size_t i = 0; i < test.length; i++) {
         result[i] = test[i] + 1;
     }
 }
