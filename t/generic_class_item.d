cd39f92f0a4afa69e6a780112d108a59f9f90d91
Emil Nicolaie Perhinschi <emilper@gmail.com>
2019-10-28 23:46:52 +0200

    ported matrix of arbitrary classes tests to TAP

---
+++ t/generic_class_item.d
@@ -0,0 +1,35 @@
+#!/usr/bin/env dub
+/+dub.json: { "dependencies": { "zug-tap": "0.1.1", "zug-data": { "path": "../" }  } } +/
+
+void main()
+{
+    import std.stdio: writeln;
+    import zug.tap;
+    import zug.matrix;
+
+    auto tap = Tap("array_utils.d");
+    tap.verbose(true);
+
+    auto orig = Matrix!TestElement(3, 3);
+    orig.set(0, 0, new TestElement());
+    auto orig_0_0 = orig.get(0, 0);
+    tap.ok(orig_0_0.data == "uninitialized");
+    tap.ok(orig.get(1,1) is null);
+    dbg(orig, "matrix initialized with TestElement class");
+
+    tap.done_testing();
+}
+
+class TestElement
+{
+    bool is_initialized = false;
+    string data = "uninitialized";
+    this()
+    {
+    }
+
+    this(string _data)
+    {
+        this.data = _data;
+    }
+}
